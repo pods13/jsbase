d8621e02e54ebcdeffdbd1077de7cf0182566c45
pods13 <alta0815@yandex.ru>
2019-09-18 15:09:03 +0300

    Completed type check exercise

---
+++ types/object-is/index.js
@@ -0,0 +1,30 @@
+if (!Object.is || true) {
+	Object.is = function ObjectIs(v1, v2) {
+		if(1/v1 === -Infinity || 1/v2 === -Infinity) {
+			return 1/v1 === -Infinity && 1/v2 === -Infinity;
+		}
+		return (Number.isNaN(v1) && Number.isNaN(v2)) || v1 === v2;
+	};
+}
+
+
+// tests:
+console.log(Object.is(42,42) === true);
+console.log(Object.is("foo","foo") === true);
+console.log(Object.is(false,false) === true);
+console.log(Object.is(null,null) === true);
+console.log(Object.is(undefined,undefined) === true);
+console.log(Object.is(NaN,NaN) === true);
+console.log(Object.is(-0,-0) === true);
+console.log(Object.is(0,0) === true);
+
+console.log(Object.is(-0,0) === false);
+console.log(Object.is(0,-0) === false);
+console.log(Object.is(0,NaN) === false);
+console.log(Object.is(NaN,0) === false);
+console.log(Object.is(42,"42") === false);
+console.log(Object.is("42",42) === false);
+console.log(Object.is("foo","bar") === false);
+console.log(Object.is(false,true) === false);
+console.log(Object.is(null,undefined) === false);
+console.log(Object.is(undefined,null) === false);
